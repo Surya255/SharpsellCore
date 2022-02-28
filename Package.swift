b0dc973bc64f5fbc474da67f807bc76366d08263
Surya <surya.saravanakumar@enparadigm.com>
2022-02-28 10:13:37 +0530

    Added swift packages to the project

--- Package.swift
+++ Package.swift
@@ -4,65 +4,70 @@
 import PackageDescription
 
 let package = Package(
-    name: "Sharpsellcore",
+    name: "SharpsellCore",
     platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Sharpsellcore",
-            targets: ["Sharpsellcore"]),
+            name: "SharpsellCore",
+            targets: ["SharpsellCore"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        //         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "8.12.1"),
+        //        .Package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "8.12.1"))
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "8.12.1"))
     ],
     targets: [
         
-//        .binaryTarget(name: "Flutter",
-//                      url: "https://github.com/Surya255/SharpsellCore/releases/download/HDFC_D_V_0.0.1/Flutter.xcframework.zip",
-//                      checksum: "ce1691fa298e1ebba84911561d3da5be45186e291f0307eaf7fc12df052cf2a3"),
-//
-//        .binaryTarget(name: "App",
-//                          url: "https://github.com/Surya255/SharpsellCore/releases/download/HDFC_D_V_0.0.1/App.xcframework.zip",
-//                          checksum: "0b6030662188e158413de04abcbcebd9dc130470ea21c250840343caccd8e46b"),
-       
+        //        .binaryTarget(name: "Flutter",
+        //                      url: "https://github.com/Surya255/SharpsellCore/releases/download/HDFC_D_V_0.0.1/Flutter.xcframework.zip",
+        //                      checksum: "ce1691fa298e1ebba84911561d3da5be45186e291f0307eaf7fc12df052cf2a3"),
+        //
+        //        .binaryTarget(name: "App",
+        //                          url: "https://github.com/Surya255/SharpsellCore/releases/download/HDFC_D_V_0.0.1/App.xcframework.zip",
+        //                          checksum: "0b6030662188e158413de04abcbcebd9dc130470ea21c250840343caccd8e46b"),
+        
         .target(
-            name: "Sharpsellcore",
+            name: "SharpsellCore",
             dependencies: [
-//                "Flutter",
+                //                "Flutter",
                 "FlutterPluginRegistrant",
-//                "App",
+                //                "App",
                 "audio_session",
                 "awesome_notifications",
                 "camera",
                 "connectivity",
                 "contacts_service",
                 "device_info_plus",
-                "FBLPromises",
+                //                                "FBLPromises",
                 "firebase_analytics",
                 "firebase_core",
                 "firebase_crashlytics",
                 "firebase_messaging",
-                "FirebaseAnalytics",
-                "FirebaseCore",
-                "FirebaseCoreDiagnostics",
-                "FirebaseCrashlytics",
-                "FirebaseInstallations",
-                "FirebaseMessaging",
+                //                                "FirebaseAnalytics",
+                //                                "FirebaseCore",
+                //                                "FirebaseCoreDiagnostics",
+                //                                "FirebaseCrashlytics",
+                //                                "FirebaseInstallations",
+                //                                "FirebaseMessaging",
+                    .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
                 "flutter_custom_tabs",
                 "flutter_inappwebview",
                 "fluttercontactpicker",
                 "fluttertoast",
                 "FMDB",
-                "GoogleAppMeasurement",
-                "GoogleDataTransport",
+                //                                "GoogleAppMeasurement",
+                //                                "GoogleDataTransport",
                 "GoogleUtilities",
                 "image_cropper",
                 "image_picker",
                 "just_audio",
                 "libwebp",
                 "light_compressor",
-                "nanopb",
+                //                "nanopb",
                 "native_pdf_renderer",
                 "open_file",
                 "OrderedSet",
@@ -88,18 +93,12 @@ let package = Package(
                 "webview_flutter_wkwebview"
                 
             ]),
-//        .testTarget(
-//            name: "SharpsellcoreTests",
-//            dependencies: ["Sharpsellcore"]),
-        
-//            .binaryTarget(name: "Flutter",
-//                          path: "artifacts/Flutter.xcframework"),
+        //        .testTarget(
+        //            name: "SharpsellcoreTests",
+        //            dependencies: ["Sharpsellcore"]),
         
             .binaryTarget(name: "FlutterPluginRegistrant",
                           path: "artifacts/FlutterPluginRegistrant.xcframework"),
-        
-//            .binaryTarget(name: "App",
-//                          path: "artifacts/App.xcframework"),
         
             .binaryTarget(name: "audio_session",
                           path: "artifacts/audio_session.xcframework"),
@@ -119,8 +118,8 @@ let package = Package(
             .binaryTarget(name: "device_info_plus",
                           path: "artifacts/device_info_plus.xcframework"),
         
-            .binaryTarget(name: "FBLPromises",
-                          path: "artifacts/FBLPromises.xcframework"),
+        //            .binaryTarget(name: "FBLPromises",
+        //                          path: "artifacts/FBLPromises.xcframework"),
         
             .binaryTarget(name: "firebase_analytics",
                           path: "artifacts/firebase_analytics.xcframework"),
@@ -133,24 +132,24 @@ let package = Package(
         
             .binaryTarget(name: "firebase_messaging",
                           path: "artifacts/firebase_messaging.xcframework"),
-        
-            .binaryTarget(name: "FirebaseAnalytics",
-                          path: "artifacts/FirebaseAnalytics.xcframework"),
-        
-            .binaryTarget(name: "FirebaseCore",
-                          path: "artifacts/FirebaseCore.xcframework"),
-        
-            .binaryTarget(name: "FirebaseCoreDiagnostics",
-                          path: "artifacts/FirebaseCoreDiagnostics.xcframework"),
-        
-            .binaryTarget(name: "FirebaseCrashlytics",
-                          path: "artifacts/FirebaseCrashlytics.xcframework"),
-        
-            .binaryTarget(name: "FirebaseInstallations",
-                          path: "artifacts/FirebaseInstallations.xcframework"),
-        
-            .binaryTarget(name: "FirebaseMessaging",
-                          path: "artifacts/FirebaseMessaging.xcframework"),
+        //
+        //            .binaryTarget(name: "FirebaseAnalytics",
+        //                          path: "artifacts/FirebaseAnalytics.xcframework"),
+        //
+        //            .binaryTarget(name: "FirebaseCore",
+        //                          path: "artifacts/FirebaseCore.xcframework"),
+        //
+        //            .binaryTarget(name: "FirebaseCoreDiagnostics",
+        //                          path: "artifacts/FirebaseCoreDiagnostics.xcframework"),
+        //
+        //            .binaryTarget(name: "FirebaseCrashlytics",
+        //                          path: "artifacts/FirebaseCrashlytics.xcframework"),
+        //
+        //            .binaryTarget(name: "FirebaseInstallations",
+        //                          path: "artifacts/FirebaseInstallations.xcframework"),
+        //
+        //            .binaryTarget(name: "FirebaseMessaging",
+        //                          path: "artifacts/FirebaseMessaging.xcframework"),
         
             .binaryTarget(name: "flutter_custom_tabs",
                           path: "artifacts/flutter_custom_tabs.xcframework"),
@@ -167,11 +166,11 @@ let package = Package(
             .binaryTarget(name: "FMDB",
                           path: "artifacts/FMDB.xcframework"),
         
-            .binaryTarget(name: "GoogleAppMeasurement",
-                          path: "artifacts/GoogleAppMeasurement.xcframework"),
-        
-            .binaryTarget(name: "GoogleDataTransport",
-                          path: "artifacts/GoogleDataTransport.xcframework"),
+        //            .binaryTarget(name: "GoogleAppMeasurement",
+        //                          path: "artifacts/GoogleAppMeasurement.xcframework"),
+        //
+        //            .binaryTarget(name: "GoogleDataTransport",
+        //                          path: "artifacts/GoogleDataTransport.xcframework"),
         
             .binaryTarget(name: "GoogleUtilities",
                           path: "artifacts/GoogleUtilities.xcframework"),
@@ -191,8 +190,8 @@ let package = Package(
             .binaryTarget(name: "light_compressor",
                           path: "artifacts/light_compressor.xcframework"),
         
-            .binaryTarget(name: "nanopb",
-                          path: "artifacts/nanopb.xcframework"),
+        //            .binaryTarget(name: "nanopb",
+        //                          path: "artifacts/nanopb.xcframework"),
         
             .binaryTarget(name: "native_pdf_renderer",
                           path: "artifacts/native_pdf_renderer.xcframework"),
